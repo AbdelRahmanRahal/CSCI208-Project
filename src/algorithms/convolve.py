99db0d9fb473acbfc8723b880ca7e4bbe798236a
AbdelRahman <abdelrahman.rahal.mail@gmail.com>
2024-05-13 00:28:47 +0300

    Added time and space complexity
    THEY COULD BE WRONG! These are just my rough estimations—not mathematically calculated.

--- src/algorithms/convolve.py
+++ src/algorithms/convolve.py
@@ -1,12 +1,16 @@
 import numpy as np
 
 def convolve(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+	'''
+	Time Complexity: O(y * x * m * n)
+	Space Complexity: O(y * x)
+	'''
 	m, n = kernel.shape
 	y, x = image.shape
 	y = y - m + 1
 	x = x - m + 1
 	new_image = np.zeros((y,x))
-
+	
 	for i in range(y):
 		for j in range(x):
 			new_image[i][j] = np.sum(image[i:i+m, j:j+m] * kernel)
