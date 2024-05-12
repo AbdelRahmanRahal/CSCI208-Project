cb7d5aae7bff0557ea63912c014fe65014d0ea07
AbdelRahman <abdelrahman.rahal.mail@gmail.com>
2024-05-13 00:47:19 +0300

    Create sobel.py
    Sobel edge detection algorithm.

---
+++ src/algorithms/sobel.py
@@ -0,0 +1,39 @@
+from .convolve import convolve
+
+import numpy as np
+from PIL import Image
+
+def sobel(image_path: str) -> Image.Image:
+	'''
+	Time complexity: O(y * x * m * n)
+	Space complexity: O(y * x)
+	'''
+	# Loading the image using PIL
+	image = Image.open(image_path).convert('L')
+	image_array = np.array(image)
+
+	# Sobel operators
+	sobel_x = np.array([
+		[-1, 0, 1],
+		[-2, 0, 2],
+		[-1, 0, 1]
+	])
+	sobel_y = np.array([
+		[-1, -2, -1],
+		[ 0,  0,  0],
+		[ 1,  2,  1]
+	])
+
+	# Applying Sobel operators
+	edges_x = convolve(image_array, sobel_x)
+	edges_y = convolve(image_array, sobel_y)
+
+	# Combining the results
+	# Magnitude of the gradient:
+	# ||G|| = sqrt(Gx^2 + Gy^2)
+	edges = np.sqrt(edges_x ** 2 + edges_y ** 2)
+
+	# Normalize to 0-255
+	edges = edges / edges.max() * 255
+
+	return Image.fromarray(edges.astype(np.uint8))
