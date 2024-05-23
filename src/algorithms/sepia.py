088feb4b5c37262812f8091e071756501a8d81e6
AbdelRahman <abdelrahman.rahal.mail@gmail.com>
2024-05-23 06:23:43 +0300

    Added docstring

--- src/algorithms/sepia.py
+++ src/algorithms/sepia.py
@@ -3,11 +3,28 @@ from typing import Union
 import numpy as np
 from PIL import Image
 
-from .convolve import convolve
 from .gamma_correction import gamma_correction
 
 
 def sepia(image: Union[str, Image.Image], gamma: float = 1) -> Image.Image:
+	'''
+	Applies a sepia filter to an input image, enhancing it with a warm brownish tone typical of old photographs.
+
+	The sepia effect is achieved by transforming the color channels of the image according to a predefined matrix,
+	followed by clipping the values to ensure they fall within the valid range for displayable colors (0-255).
+	Before applying the sepia filter, the image undergoes gamma correction to adjust its brightness and contrast.
+
+	Parameters:
+	- image (Union[str, Image.Image]): Either a file path to an image (as a string) or a PIL `Image` object.
+	  If a string is provided, the image will be loaded using PIL.
+	- gamma (float, optional): The gamma value to apply for gamma correction. Defaults to 1, which means no gamma correction is applied.
+
+	Returns:
+	- Image.Image: A PIL `Image` object representing the sepia-enhanced version of the input image.
+
+	Time complexity: O(n), where n is the number of pixels in the image.
+	Space complexity: O(n), where n is the number of pixels in the image.
+	'''
 	image = gamma_correction(image, gamma)
 	
 	image_array = np.array(image)
